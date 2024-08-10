f59afe15b04f0b9be402b9714af9ca22fd1854f3
Roman <11-roman@mail.ru>
2024-08-10 15:21:57 +0300

    изучаю синтаксис

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+// 1. создаем главный класс, с названием файла
 public class Main {
+    // 2. создаем главную функцию для запуска программы
     public static void main(String[] args) {
         System.out.println("Hello Roman!");
+        System.out.println("Привет\n\t \\ \" Hello");
     }
 }
