f05410d0be788e7a1dcc770cc6509918811a6569
Roman <11-roman@mail.ru>
2024-08-10 20:59:00 +0300

    типы данных и переменные

---
+++ src/les_1.java
@@ -0,0 +1,9 @@
+// ПЕРЕМЕННЫЕ И ТИПЫ ДАННЫХ
+
+public class les_1 {
+    public static void main(String[] args) {
+        int num = 78;
+        System.out.println(num);
+        System.out.println("My age: " + num + ".");
+    }
+}
