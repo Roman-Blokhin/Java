bbadee5a2f485cc03d73c0237fe356631defdefc
Roman <11-roman@mail.ru>
2024-08-11 15:48:04 +0300

    Получение данных от пользователя: числовой тип. Поменял структуру и модернизировал код

--- src/les_2.java
+++ src/les_2.java
@@ -1,17 +1,17 @@
-// ПОЛУЧЕНИЕ ДАННЫХ ОТ ПОЛЬЗОВАТЕЛЯ И МАТЕМАТИЧЕСКИЕ ДЕЙСТВИЯ
+// ПОЛУЧЕНИЕ ДАННЫХ ОТ ПОЛЬЗОВАТЕЛЯ
 
 import java.util.Scanner;  // 1. библиотека для получения данных от пользователя
 
 public class les_2 {
     public static void main(String[] args) {
         Scanner info = new Scanner(System.in);  // 2. создаем переменную, выделяем память под сбор данных
+
         System.out.print("Введите свое имя: ");
         String user_name = info.nextLine();  // 3. запрашиваем данные типа Строка у пользователя и записываем в переменную
-        System.out.println("Ваше имя: " + user_name);
 
-        Scanner age = new Scanner(System.in);
         System.out.print("Введите ваш возраст: ");
-        int age_num = age.nextInt();  // 3. запрашиваем данные типа Строка у пользователя и записываем в переменную
-        System.out.println("Ваш возраст: " + age_num);
+        int age = info.nextInt();  // 3. запрашиваем данные типа Число у пользователя и записываем в переменную
+
+        System.out.println("Ваше имя: " + user_name + ". Ваш возраст: "+ age);
     }
 }
