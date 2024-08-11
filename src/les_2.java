96eb998a8394bd1a95ce7da4a09d83aa0d3bb783
Roman <11-roman@mail.ru>
2024-08-11 15:34:13 +0300

    получение данных отпользователя: числовой тип

--- src/les_2.java
+++ src/les_2.java
@@ -8,5 +8,10 @@ public class les_2 {
         System.out.print("Введите свое имя: ");
         String user_name = info.nextLine();  // 3. запрашиваем данные типа Строка у пользователя и записываем в переменную
         System.out.println("Ваше имя: " + user_name);
+
+        Scanner age = new Scanner(System.in);
+        System.out.print("Введите ваш возраст: ");
+        int age_num = age.nextInt();  // 3. запрашиваем данные типа Строка у пользователя и записываем в переменную
+        System.out.println("Ваш возраст: " + age_num);
     }
 }
