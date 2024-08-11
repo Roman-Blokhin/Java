ce7836b54f79b8f2f8d2ad6f6f63d0081615d269
Roman <11-roman@mail.ru>
2024-08-11 13:35:39 +0300

    типы данных и переменные

--- src/les_1.java
+++ src/les_1.java
@@ -5,5 +5,14 @@ public class les_1 {
         int num = 78;
         System.out.println(num);
         System.out.println("My age: " + num + ".");
+
+        float num_1 = 5.098f;  // обязательно указываем в конце f
+
+        char ch = '&';  // хранит один символ в одинарных кавычках
+        String word = "Roman boolean";  // строковый формат хранения данных
+        boolean yes = true;  // булевое значение
+
+        System.out.println(num_1 + ch + word + yes);
+
     }
 }
