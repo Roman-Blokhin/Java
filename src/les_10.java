789d10c23bf270d576ade229dbaa752a3d29e374
Roman <11-roman@mail.ru>
2024-08-18 11:14:38 +0300

    Java. Цикл while

---
+++ src/les_10.java
@@ -0,0 +1,11 @@
+// Цикл while
+
+public class les_10 {
+    public static void main(String[] args) {
+        int a = 10;
+        while (a > 0){
+            a--;
+            System.out.println(a + ". Hero");
+        }
+    }
+}
