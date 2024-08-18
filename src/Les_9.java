308d6576da87bb6d6f5ecaad13395d24ad27a161
Roman <11-roman@mail.ru>
2024-08-18 09:48:29 +0300

    Java. Цикл for

---
+++ src/Les_9.java
@@ -0,0 +1,15 @@
+// Цикл for
+
+public class Les_9 {
+    public static void main(String[] args) {
+        for (int num = 0; num < 5; num ++){  // пока num < 5, прибавляем 1 и выводим на экран
+            System.out.println(num);
+        };
+
+        System.out.println("-----------");
+
+        for (int i = 100; i > 9; i/=2) {  // Цикл можно писать сокращенно - fori
+            System.out.println(i);
+        }
+    }
+}
