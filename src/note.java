aed11209e464f1d5dd16072d3c341b1cbf1dcde0
Roman <11-roman@mail.ru>
2024-08-18 20:52:33 +0300

    Java. Добавил цикл while и прописал структуру в проекте - note

--- src/note.java
+++ src/note.java
@@ -2,43 +2,81 @@ import java.util.Scanner;
 
 public class note {
     public static void main(String[] args) {
-        System.out.println("1. Архив");
-        System.out.println("2. Заметки");
-        System.out.println("3. План");
-        System.out.println("4. Новости");
-        System.out.println("5. Выход");
+        while (true) {
+            System.out.println("\n1. Архив");
+            System.out.println("2. Заметки");
+            System.out.println("3. План");
+            System.out.println("4. Новости");
+            System.out.println("5. Выход");
 
-        System.out.print("Выберите действие: ");
-        Scanner scan = new Scanner(System.in);
-        int info = scan.nextInt();
+            System.out.print("Выберите действие: ");
+            Scanner scan = new Scanner(System.in);
+            int info = scan.nextInt();
 
-        if (info == 1) {
-            System.out.println("1. Новое");
-            System.out.println("2. Старое");
-            System.out.println("3. ");
-            System.out.println("4. ");
-            System.out.println("5. ");
-        } else if (info == 2) {
-            System.out.println("1. Походы с палаткой");
-            System.out.println("2. Восхождения");
-            System.out.println("3. Морские путешествия");
-            System.out.println("4. Перелеты");
-            System.out.println("5. ");
-        } else if (info == 3) {
-            System.out.println("1. Создать маршрут");
-            System.out.println("2. Продумать отпуск");
-            System.out.println("3. Купить оборудование");
-            System.out.println("4. Посмотреть обзор");
-            System.out.println("5. Закупить провизию");
-        } else if (info == 4) {
-            System.out.println("1. Получение визы");
-            System.out.println("2. ");
-            System.out.println("3. ");
-            System.out.println("4. ");
-            System.out.println("5. ");
-        } else if (info == 5) {
-            System.out.println("Конец задачи..");
+            if (info == 1) {
+                System.out.println("\n1. Новое");
+                System.out.println("2. Старое");
+                System.out.println("3. ");
+                System.out.println("4. Назад");
+                System.out.println("5. Выход");
+
+                System.out.print("\nВыберите действие: ");
+                int num = scan.nextInt();
+                if (num == 5){
+                    System.out.println("\nПрограмма закончена.");
+                    break;
+                } else {
+                    System.out.println(" ");
+                }
+            } else if (info == 2) {
+                System.out.println("1. Походы с палаткой");
+                System.out.println("2. Восхождения");
+                System.out.println("3. Морские путешествия");
+                System.out.println("4. Назад");
+                System.out.println("5. Выход");
+
+                System.out.print("\nВыберите действие: ");
+                int num = scan.nextInt();
+                if (num == 5){
+                    System.out.println("\nПрограмма закончена.");
+                    break;
+                } else {
+                    System.out.println(" ");
+                }
+            } else if (info == 3) {
+                System.out.println("1. Создать маршрут");
+                System.out.println("2. Продумать отпуск");
+                System.out.println("3. Купить оборудование");
+                System.out.println("4. Назад");
+                System.out.println("5. Выход");
+
+                System.out.print("\nВыберите действие: ");
+                int num = scan.nextInt();
+                if (num == 5){
+                    System.out.println("\nПрограмма закончена.");
+                    break;
+                } else {
+                    System.out.println(" ");
+                }
+            } else if (info == 4) {
+                System.out.println("1. Получение визы");
+                System.out.println("2. ");
+                System.out.println("3. ");
+                System.out.println("4. Назад");
+                System.out.println("5. Выход");
+
+                System.out.print("\nВыберите действие: ");
+                int num = scan.nextInt();
+                if (num == 5){
+                    System.out.println("\nПрограмма закончена.");
+                    break;
+                } else {
+                    System.out.println(" ");
+                }
+            } else {
+                System.out.println("Конец задачи..");
+                break;
+            }
         }
-
     }
 }
