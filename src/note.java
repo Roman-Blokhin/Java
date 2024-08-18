ff709832e7ed2c8d8b7ab134bd58420b956a4f0a
Roman <11-roman@mail.ru>
2024-08-18 21:00:56 +0300

    Java. Доработал структуру цикла в проекте note

--- src/note.java
+++ src/note.java
@@ -22,14 +22,22 @@ public class note {
 
                 System.out.print("\nВыберите действие: ");
                 int num = scan.nextInt();
-                if (num == 5){
+                if (num == 1){
+                    System.out.println("\nСегодня вам расскажем про новое ..");
+                    System.out.print("\nДля возврата наберите 0: ");
+                    int num_1 = scan.nextInt();
+                } else if (num == 2){
+                    System.out.println("\nЗдесь будет старое ..");
+                    System.out.print("\nДля возврата наберите 0: ");
+                    int num_1 = scan.nextInt();
+                } else if (num == 5){
                     System.out.println("\nПрограмма закончена.");
                     break;
                 } else {
                     System.out.println(" ");
                 }
             } else if (info == 2) {
-                System.out.println("1. Походы с палаткой");
+                System.out.println("\n1. Походы с палаткой");
                 System.out.println("2. Восхождения");
                 System.out.println("3. Морские путешествия");
                 System.out.println("4. Назад");
@@ -44,7 +52,7 @@ public class note {
                     System.out.println(" ");
                 }
             } else if (info == 3) {
-                System.out.println("1. Создать маршрут");
+                System.out.println("\n1. Создать маршрут");
                 System.out.println("2. Продумать отпуск");
                 System.out.println("3. Купить оборудование");
                 System.out.println("4. Назад");
@@ -59,7 +67,7 @@ public class note {
                     System.out.println(" ");
                 }
             } else if (info == 4) {
-                System.out.println("1. Получение визы");
+                System.out.println("\n1. Получение визы");
                 System.out.println("2. ");
                 System.out.println("3. ");
                 System.out.println("4. Назад");
