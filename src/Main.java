edda5f9836e98153092b58147ebfc23cff635d5b
Roman <11-roman@mail.ru>
2024-08-19 16:56:30 +0300

    Java. Создание класса для определения свойств объекта. Вызов класса в другом файле, передача обязательных значений + замена этих значений

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
 // 1. создаем главный класс, с названием файла
 public class Main {
     // 2. создаем главную функцию для запуска программы
+    // 3. будем использовать класс для создания персонажа из другого файла
     public static void main(String[] args) {
-        System.out.println("Hello Roman!");
-        System.out.println("Привет\n\t \\ \" Hello");
+        User person_1 = new User(45, "Robert");  // обязательный ввод при создании персонажа
+        System.out.println("Создан персонаж: " + person_1.name + ". Возраст: " + person_1.age);
+
+        person_1.name = "Roman";  // изменение имени персонажа
+        person_1.age = 32;  // изменение имени персонажа
+        System.out.println("Данные изменены: ");
+        System.out.println(person_1.name);
+        System.out.println(person_1.age);
     }
 }
---
+++ src/User.java
@@ -0,0 +1,14 @@
+// Объекты
+// Создаем класс, где указываем два свойства для нашего объекта
+
+public class User {
+
+    String name;
+    int age;
+
+
+    public User(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+}
