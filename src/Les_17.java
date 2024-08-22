f22a9fb157b830a9cc9bf864b560b6f163c468b7
Roman <11-roman@mail.ru>
2024-08-22 21:22:58 +0300

    Java. Ключевое слово final

---
+++ src/Les_17.java
@@ -0,0 +1,9 @@
+// ключевое слово - final
+
+public class Les_17 {
+    public static void main(String[] args) {
+        final int AGE = 90;  // final задает константу - финальное значение
+
+        System.out.println(AGE);
+    }
+}
