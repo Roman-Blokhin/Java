0b8bc2455845005eb14cb7a8a2b33ec488537357
Roman <11-roman@mail.ru>
2024-08-26 08:45:03 +0300

    Java. Новый файл для задач

---
+++ src/Cats.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Cats {
+    public static void main(String[] args) {
+
+        // ЗАДАЧА 31 - Перебор свойств объекта с помощью рефлексии.
+        // Задание: Используйте рефлексию для вывода всех полей объекта класса Cat
+
+
+
+    }
+}
